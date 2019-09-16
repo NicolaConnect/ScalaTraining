f5b3515253a8b94e3d3f4bfd0100bef7b9117ce9
Nicola Delle Foglie <nicola.dellefoglie@devlounge.it>
2019-09-16 21:52:15 +0200

    Added a way to calculate oldest person in a group of people (ages only)

--- src/main/scala/oldest_person/Main.scala
+++ src/main/scala/oldest_person/Main.scala
@@ -1,6 +1,28 @@
 package oldest_person
 
+import scala.collection.mutable.ListBuffer
+import scala.io.StdIn
+
 object Main extends App {
-  val ages = Seq(42, 167, 29, 64)
-  println(s"The oldest person is ${ages.max}")
+  val numbers = (('0' to '9')).toSet
+  def containsNumbersOnly(inputString: String) = inputString.forall(numbers.contains(_))
+
+  var ages = new ListBuffer[Int]()
+  var ageRead: String = ""
+
+  do {
+    print("Please, input the age of a new person or input an empty line to calculate the maximum: ")
+    ageRead = StdIn.readLine()
+
+    if(containsNumbersOnly(ageRead) && ageRead != "") {
+      ages += ageRead.toInt
+    } else {
+      print("Looks like something went wrong. Please, make sure that you input numbers only. ")
+    }
+  } while(ageRead != "")
+
+  print("\n\n---\n\n")
+
+  println(s"You inputted ${ages.length} ages. The maximum was ${ages.max}.")
+  println(s"That means that the oldest person is ${ages.max} years old.")
 }
